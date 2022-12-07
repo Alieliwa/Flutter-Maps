a4103a943f4e0c608b67291598b691e002c4b91a
AliEliwa <alieliwa515>
2022-12-08 01:05:22 +0200

    add Polygon

--- lib/presentaion/homescreen.dart
+++ lib/presentaion/homescreen.dart
@@ -6,13 +6,40 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   var myMarkers = HashSet<Marker>();
+  // late BitmapDescriptor customMarker;
+
+  // getCustomMarker() async {
+  //   customMarker = await BitmapDescriptor.fromAssetImage(
+  //       ImageConfiguration.empty, "assets/images/convocation.jpg");
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getCustomMarker();
+  // }
+  Set<Polygon> myPolygon() {
+    List<LatLng> polygonCoords =  [];
+    polygonCoords.add(const LatLng(37.43296265331129, -122.08832357078792));
+    polygonCoords.add(const LatLng(37.43006265331129, -122.08832357078792));
+    polygonCoords.add(const LatLng(37.43006265331129, -122.08332357078792));
+    polygonCoords.add(const LatLng(37.43296265331129, -122.08832357078792));
+
+    Set<Polygon> polygonSet =  Set();
+    polygonSet.add(Polygon(
+        polygonId: const PolygonId('test'),
+        points: polygonCoords,
+        strokeWidth: 2,
+        strokeColor: Colors.red));
+    return polygonSet;
+  }
+
 
 
   @override
@@ -22,26 +49,28 @@ class _HomeScreenState extends State<HomeScreen> {
         body: GoogleMap(
           mapType: MapType.hybrid,
           initialCameraPosition: const CameraPosition(
-            target: LatLng(30.136738, 31.326208),
-            zoom: 19,
+            target: LatLng(37.43296265331129, -122.08832357078792),
+            zoom: 15,
           ),
           onMapCreated: (GoogleMapController controller) {
-            setState(() {
-              myMarkers.add( Marker(markerId: const MarkerId('1'),
-                  position: const LatLng(30.136738, 31.326208),
-                  infoWindow: InfoWindow(
-                    title: 'Ali ELiwa',
-                    snippet: 'Welcome every body',
-                    onTap:(){
-                      print('you clickedb,afllkfmrekafoierjfija');
-                    },
-                  ) ,
-              ),
-              );
-            },);
+            setState(
+              () {
+                myMarkers.add(
+                   const Marker(
+                    markerId: MarkerId('1'),
+                    position: LatLng(30.136738, 31.326208),
+                    infoWindow: InfoWindow(
+                      title: 'Ali ELiwa',
+                      snippet: 'Welcome every body',
+                    ),
+                    // icon: customMarker,
+                  ),
+                );
+              },
+            );
           },
           markers: myMarkers,
-
+          polygons: myPolygon(),
         ),
       ),
     );
